406bbbc9b427103946b2130279400f0a2bb3cfc6
flosell <florian.sellmayr@gmail.com>
2014-09-14 12:59:14 +0200

    updated project-version to match current library version, added description in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lambdacd/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject lambdacd/lein-template "0.1.0-alpha2"
+  :description "leiningen template for lambdacd"
   :url "http://github.com/flosell/lambdacd-template"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
