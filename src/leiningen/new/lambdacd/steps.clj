9f50f1cc7871267f3ffcecbd90b1042eb14c2698
flosell <florian.sellmayr@gmail.com>
2015-01-03 12:30:48 +0100

    using 0.1.0-alpha8

--- src/leiningen/new/lambdacd/steps.clj
+++ src/leiningen/new/lambdacd/steps.clj
@@ -4,10 +4,10 @@
 (defn some-step-that-does-nothing [& _]
   {:status :success})
 
-(defn some-step-that-echos-foo [& _]
-  (shell/bash "/" "echo foo"))
-(defn some-step-that-echos-bar [& _]
-  (shell/bash "/" "echo bar"))
+(defn some-step-that-echos-foo [_ ctx]
+  (shell/bash ctx "/" "echo foo"))
+(defn some-step-that-echos-bar [_ ctx]
+  (shell/bash ctx "/" "echo bar"))
 
-(defn some-failing-step [& _]
-  (shell/bash "/" "echo \"i am going to fail now...\"" "exit 1"))
+(defn some-failing-step [_ ctx]
+  (shell/bash ctx "/" "echo \"i am going to fail now...\"" "exit 1"))
