ed8d5a4a4cd16f24c032ab19dec1b86d17554e9e
flosell <florian.sellmayr@gmail.com>
2015-01-04 16:47:45 +0100

    fixing `lein ring uberjar`

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -9,4 +9,6 @@
                            [ch.qos.logback/logback-classic "1.0.13"]]
             :ring {:handler {{name}}.pipeline/app
                    :init {{name}}.pipeline/start-pipeline-thread }
-            :plugins [[lein-ring "0.8.11"]])
+            :profiles {:uberjar {:aot [{{name}}.pipeline.main]}}
+
+            :plugins [[lein-ring "0.8.13"]])
