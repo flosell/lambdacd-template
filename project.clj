321571df0fcd5136356d8200ec4788fc6568907d
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-03-29 20:47:40 +0200

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd/lein-template "0.1.1"
+(defproject lambdacd/lein-template "0.1.2-SNAPSHOT"
   :description "leiningen template for lambdacd"
   :url "http://github.com/flosell/lambdacd-template"
   :license {:name "Apache License, version 2.0"
