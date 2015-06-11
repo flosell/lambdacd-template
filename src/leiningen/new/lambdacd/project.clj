bdeb06e8f818a847674a0263aef28224e6693104
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-06-11 21:21:13 +0200

    use 0.3.1 of lambdacd

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.3.0"]
+            :dependencies [[lambdacd "0.3.1"]
                            [ring-server "0.3.1"]
                            [org.clojure/clojure "1.6.0"]
                            [org.clojure/tools.logging "0.3.0"]
