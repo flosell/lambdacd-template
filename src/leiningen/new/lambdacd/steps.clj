8fefb6eb5a2805dc91a4a732024181acbd566455
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-09-03 14:07:19 +0200

    add arg and ctx everywhere for more consistency

--- src/leiningen/new/lambdacd/steps.clj
+++ src/leiningen/new/lambdacd/steps.clj
@@ -1,14 +1,14 @@
 (ns {{name}}.steps
   (:require [lambdacd.steps.shell :as shell]))
 
-(defn some-step-that-does-nothing [& _]
+(defn some-step-that-does-nothing [arg ctx]
   {:status :success})
 
-(defn some-step-that-echos-foo [_ ctx]
+(defn some-step-that-echos-foo [arg ctx]
   (shell/bash ctx "/" "echo foo"))
 
-(defn some-step-that-echos-bar [_ ctx]
+(defn some-step-that-echos-bar [arg ctx]
   (shell/bash ctx "/" "echo bar"))
 
-(defn some-failing-step [_ ctx]
+(defn some-failing-step [arg ctx]
   (shell/bash ctx "/" "echo \"i am going to fail now...\"" "exit 1"))
