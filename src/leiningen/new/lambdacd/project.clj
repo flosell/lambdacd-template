86299b0263c28c11f0818724fff105919046c739
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-10-04 16:45:47 +0200

    use lambdacd-0.5.5

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,9 +1,9 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.5.3"]
+            :dependencies [[lambdacd "0.5.5"]
                            [ring-server "0.3.1"]
-                           [org.clojure/clojure "1.6.0"]
+                           [org.clojure/clojure "1.7.0"]
                            [org.clojure/tools.logging "0.3.0"]
                            [org.slf4j/slf4j-api "1.7.5"]
                            [ch.qos.logback/logback-core "1.0.13"]
