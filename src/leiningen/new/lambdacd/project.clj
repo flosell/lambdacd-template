08b7eaef573726e9003eb1aa5a4f3c33cfc0eb76
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-03-29 07:44:42 +0200

    Bump LambdaCD and plugin versions

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,10 +1,10 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.12.1"]
-                           [ring-server "0.3.1"]
+            :dependencies [[lambdacd "0.13.1"]
+                           [ring-server "0.4.0"]
                            [org.clojure/clojure "1.7.0"]
-                           [org.clojure/tools.logging "0.3.0"]
+                           [org.clojure/tools.logging "0.3.1"]
                            [org.slf4j/slf4j-api "1.7.5"]
                            [ch.qos.logback/logback-core "1.0.13"]
                            [ch.qos.logback/logback-classic "1.0.13"]]
