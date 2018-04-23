566bcdffa3dc7c9c74f230a82d27b099b6f6c49f
Florian Sellmayr <florian.sellmayr@gmail.com>
2018-04-23 08:24:24 +0700

    Bump versions of LambdaCD, LambdaUI and Clojure to be fully up to date again #1

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,10 +1,10 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.13.5"]
-                           [lambdaui "1.0.0"]
+            :dependencies [[lambdacd "0.14.0"]
+                           [lambdaui "1.1.0"]
                            [http-kit "2.2.0"]
-                           [org.clojure/clojure "1.7.0"]
+                           [org.clojure/clojure "1.9.0"]
                            [org.clojure/tools.logging "0.3.1"]
                            [org.slf4j/slf4j-api "1.7.5"]
                            [ch.qos.logback/logback-core "1.0.13"]
