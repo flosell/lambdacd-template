3e9b341b5c853c3594139c1b2e6a9772c4a17e91
Florian Sellmayr <florian.sellmayr@gmail.com>
2019-12-21 19:07:41 +0700

    Bump lambdacd to latest version

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.14.0"]
+            :dependencies [[lambdacd "0.14.4"]
                            [lambdaui "1.1.0"]
                            [http-kit "2.3.0"]
                            [org.clojure/clojure "1.9.0"]
