84324a43d9f24f83cef14b3e53e93623f0bf520b
Florian Sellmayr <florian.sellmayr@gmail.com>
2021-06-12 13:06:56 +0700

    Bump version to 0.14.7

--- src/leiningen/new/lambdacd/project.clj
+++ src/leiningen/new/lambdacd/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :dependencies [[lambdacd "0.14.6"]
+            :dependencies [[lambdacd "0.14.7"]
                            [lambdaui "1.1.0" :exclusions [lambdacd]]
                            [http-kit "2.3.0"]
                            [org.clojure/clojure "1.9.0"]
